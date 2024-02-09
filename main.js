ecad3d7796d1df41aed10dc73ea6d140b9c21784
abdelrahmanrgab <127410127+abdelrahmanrgab@users.noreply.github.com>
2024-02-09 21:28:40 +0200

    add Aos lib

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ const showlinks = document.querySelector(".links");
 showlinks.onclick = ()=>{
   showlinks.classList.toggle("active");
 }
+  AOS.init({
+    duration: 800,
+    easing: 'ease-in-out',
+    once: true,
+  });
